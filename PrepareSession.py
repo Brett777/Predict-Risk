6d835bcd199ce306e59eb4b565f8f6aad34304de
Brett777 <brett@datascience.com>
2017-06-02 20:20:37 +0000

    Sync from DataScience jupyter

---
+++ PrepareSession.py
@@ -0,0 +1,4 @@
+import os
+os.system("sudo apt-get update")
+os.system('sudo apt-get install openjdk-8-jre -y')
+os.system('pip install http://h2o-release.s3.amazonaws.com/h2o/master/3901/Python/h2o-3.11.0.3901-py2.py3-none-any.whl')
