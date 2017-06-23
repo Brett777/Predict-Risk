61217d9f502faecee61d312acc91e64edad84629
Brett777 <brett@datascience.com>
2017-06-23 06:26:23 +0000

    Sync from DataScience jupyter

--- PrepareSession.py
+++ PrepareSession.py
@@ -1,6 +1,6 @@
 import os
 os.system("sudo apt-get update")
 os.system('sudo apt-get install openjdk-8-jre -y')
-os.system('pip install http://h2o-release.s3.amazonaws.com/h2o/master/3901/Python/h2o-3.11.0.3901-py2.py3-none-any.whl')
+os.system('pip install http://s3.amazonaws.com/h2o-deepwater/public/nightly/latest/h2o-3.11.0-py2.py3-none-any.whl')
 os.system('pip install plotly')
 os.system('pip install cufflinks')
