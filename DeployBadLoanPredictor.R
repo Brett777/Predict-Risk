43d4dd27c1691be77cf459d2ba72908dc44113f3
Brett777 <brett@datascience.com>
2017-06-28 04:09:53 +0000

    Sync from DataScience rstudio

--- DeployBadLoanPredictor.R
+++ DeployBadLoanPredictor.R
@@ -5,44 +5,27 @@ h2o.init(nthreads = 1, max_mem_size = "1500m")
 
 approveLoan <- function(Loan_Amount,Term,Interest_Rate,Employment_Years,Home_Ownership,Annual_Income,Verification_Status,Loan_Purpose,State,
                         Debt_to_Income,Delinquent_2yr,Revolving_Cr_Util,Total_Accounts,Longest_Credit_Length){
-
-  loanApplication <- data.frame('Loan_Amount' = Loan_Amount,
-                                'Term' = Term,
-                                'Interest_Rate' = Interest_Rate, 
-                                'Employment_Years' = Employment_Years,
-                                'Home_Ownership' = Home_Ownership,
-                                'Annual_Income' = Annual_Income,
-                                'Verification_Status'= Verification_Status,
-                                'Loan_Purpose' = Loan_Purpose,
-                                'State' = State,
-                                'Debt_to_Income' = Debt_to_Income,
-                                'Delinquent_2yr' = Delinquent_2yr,
-                                'Revolving_Cr_Util' = Revolving_Cr_Util,
-                                'Total_Accounts' = Total_Accounts,
-                                'Longest_Credit_Length' = Longest_Credit_Length)
+  h2o.connect()
+  h2o.removeAll()
+ 
+  #loanApplication <- data.frame('Loan_Amount' = Loan_Amount,
+  #                              'Term' = Term,
+  #                              'Interest_Rate' = Interest_Rate, 
+  #                              'Employment_Years' = Employment_Years,
+  #                              'Home_Ownership' = Home_Ownership,
+  #                              'Annual_Income' = Annual_Income,
+  #                              'Verification_Status'= Verification_Status,
+  #                              'Loan_Purpose' = Loan_Purpose,
+  #                              'State' = State,
+  #                              'Debt_to_Income' = Debt_to_Income,
+  #                              'Delinquent_2yr' = Delinquent_2yr,
+  #                              'Revolving_Cr_Util' = Revolving_Cr_Util,
+  #                              'Total_Accounts' = Total_Accounts,
+  #                              'Longest_Credit_Length' = Longest_Credit_Length)
   
-  newLoanApplicationH2O.hex = as.h2o(x = loanApplication, destination_frame = "newLoanApplicationH2O.hex")
+  #newLoanApplicationH2O.hex = as.h2o(x = loanApplication, destination_frame = "newLoanApplicationH2O.hex")
+  newLoanApplicationH2O = h2o.importFile(path = "loanApplication.csv")
   loanApprover <- h2o.loadModel(path = "LoanApprover.model")
-  prediction = h2o.predict(object = loanApprover, newdata = newLoanApplicationH2O.hex)
+  prediction = h2o.predict(object = loanApprover, newdata = newLoanApplicationH2O)
  return(prediction)
 }
-
-# Sample data to test the function
-
-Loan_Amount = 5000
-Term = "60 months"
-Interest_Rate=13
-Employment_Years=5
-Home_Ownership="RENT"
-Annual_Income=75000
-Verification_Status="VERIFIED - income"
-Loan_Purpose="credit_card"
-State="CA"
-Debt_to_Income=16.12
-Delinquent_2yr=0
-Revolving_Cr_Util=37
-Total_Accounts=6
-Longest_Credit_Length=97
-
-
-approveLoan(Loan_Amount,Term,Interest_Rate,Employment_Years,Home_Ownership,Annual_Income,Verification_Status,Loan_Purpose,State,Debt_to_Income,Delinquent_2yr,Revolving_Cr_Util,Total_Accounts,Longest_Credit_Length)
