c8825386945e4919b699467907f815e0b020a91a
Brett777 <brett@datascience.com>
2017-06-28 03:15:23 +0000

    Sync from DataScience rstudio

--- DeployBadLoanPredictor.R
+++ DeployBadLoanPredictor.R
@@ -7,23 +7,23 @@ approveLoan <- function(Loan_Amount,Term,Interest_Rate,Employment_Years,Home_Own
                         Debt_to_Income,Delinquent_2yr,Revolving_Cr_Util,Total_Accounts,Longest_Credit_Length){
 
   loanApplication <- data.frame('Loan_Amount' = Loan_Amount,
-                                   'Term' = Term,
-                                   'Interest_Rate' = Interest_Rate, 
-                                   'Employment_Years' = Employment_Years,
-                                   'Home_Ownership' = Home_Ownership,
-                                   'Annual_Income' = Annual_Income,
-                                   'Verification_Status'= Verification_Status,
-                                   'Loan_Purpose' = Loan_Purpose,
-                                   'State' = State,
-                                   'Debt_to_Income' = Debt_to_Income,
-                                   'Delinquent_2yr' = Delinquent_2yr,
-                                   'Revolving_Cr_Util' = Revolving_Cr_Util,
-                                   'Total_Accounts' = Total_Accounts,
-                                   'Longest_Credit_Length' = Longest_Credit_Length)
+                                'Term' = Term,
+                                'Interest_Rate' = Interest_Rate, 
+                                'Employment_Years' = Employment_Years,
+                                'Home_Ownership' = Home_Ownership,
+                                'Annual_Income' = Annual_Income,
+                                'Verification_Status'= Verification_Status,
+                                'Loan_Purpose' = Loan_Purpose,
+                                'State' = State,
+                                'Debt_to_Income' = Debt_to_Income,
+                                'Delinquent_2yr' = Delinquent_2yr,
+                                'Revolving_Cr_Util' = Revolving_Cr_Util,
+                                'Total_Accounts' = Total_Accounts,
+                                'Longest_Credit_Length' = Longest_Credit_Length)
   
-  newLoanApplicationH2O <- as.h2o(loanApplication)
+  newLoanApplicationH2O.hex <- as.h2o(loanApplication)
   loanApprover <- h2o.loadModel(path = "LoanApprover.model")
-  prediction = h2o.predict(object = loanApprover, newdata = newLoanApplicationH2O)
+  prediction = h2o.predict(object = loanApprover, newdata = newLoanApplicationH2O.hex)
  return(prediction)
 }
 
